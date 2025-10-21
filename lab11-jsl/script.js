27c33320e9e2ad2db2de64cbf091a922642ed465
beasorinio <beasorinio9@gmail.com>
2025-10-21 11:13:09 +0800

    edit

---
+++ lab11-jsl/script.js
@@ -0,0 +1,13 @@
+// A variable declaration using let
+let number1 = 10;
+let number2 = 5;
+
+// A math operation using two variables
+let sum = number1 + number2;
+
+// A simple console.log() output
+console.log("The first number is:", number1);
+console.log("The second number is:", number2);
+console.log("The sum of the two numbers is:", sum);
+
+
